1faf6f6e30e0256ea8289030519d9ca85e0b6081
FengWu <2000013727@stu.pku.edu.cn>
2023-04-14 23:54:43 +0800

    timeout

--- kernel/trap.c
+++ kernel/trap.c
@@ -67,7 +67,14 @@ usertrap(void)
     syscall();
   } else if((which_dev = devintr()) != 0){
     // ok
-  } else {
+  } else if(r_scause() == 15) { // 缺页错误
+    if(uncopied_cow(p->pagetable,r_stval()) > 0) {
+        if (r_stval() < PGSIZE)  //对0起始地址等低地址直接写，那么直接退出
+            p->killed = 1;
+        if (cowalloc(p->pagetable, r_stval()) < 0)
+            p->killed = 1;
+    }
+  }else {
     printf("usertrap(): unexpected scause %p pid=%d\n", r_scause(), p->pid);
     printf("            sepc=%p stval=%p\n", r_sepc(), r_stval());
     setkilled(p);
@@ -215,7 +222,7 @@ devintr()
 
     return 2;
   } else {
-    return 0;
+      return 0;
   }
 }
 
